d82c9ab7f939d4cb958fdbfb8a103c9ab47269ac
OleksiyLavreshyn <alexxxx44442@gmail.com>
2026-08-27 23:49:19 +0300

    fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -4,27 +4,52 @@ module.exports = [
     {
         ignores: [
             'node_modules/**',
-            'coverage/**',
             'allure-results/**',
             'allure-report/**',
             'reports/**',
+            'screenshots/**',
+            'videos/**',
         ],
     },
+
     js.configs.recommended,
+
     {
+        files: ['eslint.config.js'],
+
+        languageOptions: {
+            globals: {
+                require: 'readonly',
+                module: 'readonly',
+                console: 'readonly',
+            },
+        },
+    },
+
+    {
+        files: ['src/**/*.js'],
+
         languageOptions: {
             ecmaVersion: 'latest',
             sourceType: 'commonjs',
+
             globals: {
+                browser: 'readonly',
+                $: 'readonly',
+                $$: 'readonly',
+
+                describe: 'readonly',
+                it: 'readonly',
+                before: 'readonly',
+                beforeEach: 'readonly',
+                after: 'readonly',
+                afterEach: 'readonly',
+
+                expect: 'readonly',
                 console: 'readonly',
-                process: 'readonly',
-                require: 'readonly',
-                module: 'readonly',
-                __dirname: 'readonly',
-                __filename: 'readonly',
-                fetch: 'readonly',
             },
         },
+
         rules: {
             'no-unused-vars': 'warn',
             'no-undef': 'error',
